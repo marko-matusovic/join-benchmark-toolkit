3e549483644c06dd8a8d149c91fba2b15f2748e5
Marko Matušovič <matusovic.m@gmail.com>
2024-03-23 18:02:46 +0100

    Style

--- jb-environment/join-benchmark/benchmark/run/individual/main_plot_query_times.py
+++ jb-environment/join-benchmark/benchmark/run/individual/main_plot_query_times.py
@@ -1,3 +1,4 @@
+from matplotlib.ticker import FuncFormatter
 import numpy as np
 import pandas as pd
 from benchmark.tools.tools import ensure_dir
@@ -83,7 +84,6 @@ def main(
             *[np.std([row[jn] for row in df_g_q["JOINS"]]) for jn in range(n_join)],
         ]
         
-
         # ======================== PLOTTING ========================
 
         plt.figure(figsize=(10, 8))
@@ -92,11 +92,8 @@ def main(
         # Create a list of indices for the x-axis
         indices = range(len(keys))
 
-        # Plot the real values on the first y-axis
-        # ax1 = plt.bar(indices, real_values, width, label="Real", color="#1f77b4", alpha=0.9)
-        # plt.ylabel('Real Values')
+        # Plot CPU means with error bars on the first y-axis
         ax1 = plt.gca()
-        # Plot CPU means with error bars
         ax1.bar(
             indices,
             means_cpu,
@@ -108,16 +105,8 @@ def main(
             capsize=5,  # Optional: adds caps to the error bars
         )
 
-        all_values = means_cpu + means_gpu
-        all_stds = stds_cpu + stds_gpu
-        global_min = min([val - std for val, std in zip(all_values, all_stds)])
-        global_max = max([val + std for val, std in zip(all_values, all_stds)])
-
-        # Create a second y-axis that shares the same x-axis
-        ax2 = plt.twinx()
-
-        # Plot the predicted values on the second y-axis
-        ax2.bar(
+        # Plot GPU means with error bars, slightly offset on the x-axis for clarity
+        ax1.bar(
             [i + width for i in indices],
             means_gpu,
             width,
@@ -127,26 +116,30 @@ def main(
             alpha=0.9,
             capsize=5,  # Optional: adds caps to the error bars
         )
-        
+
+        # Adjust the y-axis scale if needed
         ax1.set_yscale("log")
-        ax1.set_ylim([global_min, global_max])
-        ax2.set_yscale("log")
-        ax2.set_ylim(global_min, global_max)
-        
-        # ax2.set_ylabel("GPU")
+        ax1.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: '{:}'.format(x)))
+
+
+        # Set the x-axis ticks and labels
         ax1.set_xticks([i + width / 2 for i in indices])
         ax1.set_xticklabels(keys, rotation=30)
 
+        # Set the x-axis label and add the legend
         ax1.set_xlabel("Stages")
+        ax1.set_ylabel("Execution Time [s] log")
         ax1.legend(loc='upper left', bbox_to_anchor=(0.01, 0.99))
-        ax2.legend(loc='upper left', bbox_to_anchor=(0.01, 0.94))
-        
+
+        # Set the plot title
         plt.title("Execution Time for Stages of Query: " + query)
-        
+
+        # Save the plot
         dir_path = f"{res_path}/figs/query-details/{query}.png"
         ensure_dir(dir_path)
         plt.savefig(dir_path)
 
+        # Close the plot to free up memory
         plt.close()
 
     print("Done!")
